3793bb7415dcc6a10d94a7b66a2bb77fef93158a
j_yoldoshov <J.Yo'ldoshov@uzinfocom.uz>
2025-02-11 11:26:25 +0500

    solve: implemented Clear Digits problem solution

---
+++ src/main/java/com/example/months/february/Day10.java
@@ -0,0 +1,33 @@
+package com.example.months.february;
+
+import java.util.Stack;
+
+public class Day10 {
+  public String clearDigits(String s) {
+
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (Character.isDigit(c)) {
+        if (!stack.isEmpty()){
+          stack.pop();
+        }
+      }else {
+        stack.push(c);
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop());
+    }
+
+    return sb.reverse().toString();
+  }
+
+  public static void main(String[] args) {
+    Day10 solution = new Day10();
+    String s = "abss123";
+    String result = solution.clearDigits(s);
+    System.out.println(result);
+  }
+}
