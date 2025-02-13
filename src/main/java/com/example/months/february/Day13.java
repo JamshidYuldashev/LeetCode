4bbd84dd3a985c4a9cae5028f6d8fd7b00db4cf5
j_yoldoshov <J.Yo'ldoshov@uzinfocom.uz>
2025-02-13 14:15:55 +0500

    solve: implemented minimum-operations-to-exceed-threshold-value-ii problem solution

---
+++ src/main/java/com/example/months/february/Day13.java
@@ -0,0 +1,29 @@
+package com.example.months.february;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/
+public class Day13 {
+
+  public int minOperations(int[] nums, int k) {
+    PriorityQueue<Long> pq = new PriorityQueue<>();
+    for (int x : nums) {
+      pq.offer((long) x);
+    }
+    int ans = 0;
+    for (; pq.size() > 1 && pq.peek() < k; ++ans) {
+      long x = pq.poll(), y = pq.poll();
+      pq.offer(x * 2 + y);
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Day13 solution = new Day13();
+    int[] nums = {2,11,10,1,3}; // 2
+    int k = 5;
+    int result = solution.minOperations(nums, k);
+    System.out.println(result);
+  }
+}
